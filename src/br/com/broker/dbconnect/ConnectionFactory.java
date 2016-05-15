8875727796893de5cbe065383ead23f71f3e6e44
Lucas Santos dos Anjos <lucassantosdosanjos@yahoo.com>
2016-05-15 18:54:38 -0300

    Update ConnectionFactory.java

--- src/br/com/broker/dbconnect/ConnectionFactory.java
+++ src/br/com/broker/dbconnect/ConnectionFactory.java
@@ -11,7 +11,7 @@ public class ConnectionFactory {
 	public Connection getConnection(){
 		try{
 			DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-			return DriverManager.getConnection("jdbc:mysql://localhost/broker","root","JUDAHLIVE");			
+			return DriverManager.getConnection("jdbc:mysql://localhost/broker","root","------");			
 		}catch (SQLException e){
 			throw new RuntimeException(e);
 			
